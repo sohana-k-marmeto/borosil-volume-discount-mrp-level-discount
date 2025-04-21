00d7fdd6e1ae8f4ad44540f180bbd9cab2a7db4f
sohana-k-marmeto <sohana.k@marmeto.com>
2025-04-21 16:02:10 +0530

    Added an identifier for volumeDiscount product to prevent extra discount getting applied

--- extensions/product-discount/src/run.js
+++ extensions/product-discount/src/run.js
@@ -17,38 +17,46 @@ export function run(input) {
   const discounts = [];
   const lines = input?.cart?.lines || [];
   lines.forEach((line) => {
+    let isVolumeDiscountProduct = line?.volumeDiscountProduct?.value;
+    if (isVolumeDiscountProduct == "false") {
+      return;
+    }
     if (line?.merchandise?.__typename === "ProductVariant") {
       let metafieldValue = line.merchandise.product.metafield?.value;
-  
+
       // Ensure metafieldValue is valid and parse it as JSON
       let parsedValues = metafieldValue ? JSON.parse(metafieldValue) : [];
-    
+
       parsedValues.forEach((item) => {
         let [volumeDiscountQuantity, discountAmount] = item.split(",");
         // Convert to numbers for comparison
         volumeDiscountQuantity = Number(volumeDiscountQuantity) || 0;
-        let parsedDiscountAmount = Number(discountAmount) || 0; 
+        let parsedDiscountAmount = Number(discountAmount) || 0;
         let sellingPrice = line?.cost?.amountPerQuantity?.amount;
         let compareAtPrice = line?.cost?.compareAtAmountPerQuantity?.amount;
         let requiredDiscountAmount = 0; // Initialize with 0
-  
+
         if (volumeDiscountQuantity === line.quantity) {
           let discountAmountForMRP =
-            compareAtPrice - (compareAtPrice * parsedDiscountAmount) / 100; 
+            compareAtPrice - (compareAtPrice * parsedDiscountAmount) / 100;
           if (sellingPrice > discountAmountForMRP) {
             requiredDiscountAmount = sellingPrice - discountAmountForMRP;
           }
         }
-  
+
         let parsedRequiredDiscount = Number(requiredDiscountAmount) || 0;
-  
+
         // Multiply discount by quantity
         let totalDiscountAmount = parsedRequiredDiscount * line.quantity;
-  
+
         // Apply discount if the quantity matches
-        if (volumeDiscountQuantity === line.quantity && totalDiscountAmount > 0) {
+        if (
+          volumeDiscountQuantity === line.quantity &&
+          totalDiscountAmount > 0 &&
+          isVolumeDiscountProduct == "true"
+        ) {
           discounts.push({
-            message: `Discount Applied`,
+            message: `Volume Discount Applied`,
             targets: [
               {
                 cartLine: {
@@ -66,7 +74,6 @@ export function run(input) {
       });
     }
   });
-  
 
   return discounts.length > 0
     ? {
